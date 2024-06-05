07f592d86608fa48710530b3e31d3c490f896f2a
zoeznm <bomikim046@gmail.com>
2024-06-05 12:56:36 +0900

    완료

--- imsi.js
+++ imsi.js
@@ -10,13 +10,6 @@ const endpoint = `http://apis.data.go.kr/6300000/eventDataService`;
 
 const keyName = `BOSIHO7gl1tSe4mkEEfU0TErgpn97yFSS8q7aBAJ%2FWkDdRB4F450fg8Sfts8jfBjIxx69ea%2FcxlSpFzL77XWMQ%3D%3D`;
 const example = `
-http://apis.data.go.kr/6300000/eventDataService/eventDataListJson
-?
-serviceKey=${keyName}
-&
-numOfRows=10
-&
-pageNo=1
-`;
+http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=${keyName}&numOfRows=10&pageNo=1`;
 
 console.log(example);
